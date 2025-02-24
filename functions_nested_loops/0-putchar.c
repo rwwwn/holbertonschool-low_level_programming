83b51efcc3f60b4d719486dd454c62d5d3968471
rwwwn <ralbaraiki@gmail.com>
2025-02-24 16:26:50 +0000

    Added 0-putchar.c program

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n'); /* Print newline */
+
+	return (0);
+}
