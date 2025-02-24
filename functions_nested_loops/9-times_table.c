3a3a25d2a187f9263dea3a691641eb4c004f5a7c
rwwwn <ralbaraiki@gmail.com>
2025-02-24 16:56:55 +0000

    Fixed Betty style issues for 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,26 +5,26 @@
  */
 void times_table(void)
 {
-    int row, col, product;
+	int row, col, product;
 
-    for (row = 0; row < 10; row++)
-    {
-        for (col = 0; col < 10; col++)
-        {
-            product = row * col;
-            if (col == 0)
-                _putchar('0' + product);
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (product < 10)
-                    _putchar(' ');
-                if (product >= 10)
-                    _putchar('0' + (product / 10));
-                _putchar('0' + (product % 10));
-            }
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			product = row * col;
+			if (col == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				if (product >= 10)
+					_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
