1bbb50a01b22571c37fc7dcebc3f7b239c444d7b
rwwwn <ralbaraiki@gmail.com>
2025-02-25 13:21:54 +0000

    Fixed Betty style for print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers from 0 to 9 except 2 and 4
- *
- * Return: Nothing (void)
+ * print_most_numbers - Prints numbers 0-9 except 2 and 4.
  */
 void print_most_numbers(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        if (num != 2 && num != 4)
-            _putchar(num + '0');
-    }
-    _putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar(num + '0');
+	}
+	_putchar('\n');
 }
