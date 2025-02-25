e88e07ac81431b7575640a825d8fc12855d1311c
rwwwn <ralbaraiki@gmail.com>
2025-02-25 13:44:49 +0000

    Added function to print a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of '#' characters
+ * @size: The height and width of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, col;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			if (col <= size - row)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
