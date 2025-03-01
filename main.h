0c8e29e12b1cc3a66aace8c045180a1699351b74
rwwwn <ralbaraiki@gmail.com>
2025-03-01 20:26:23 +0000

    Added leet function with Betty style compliance

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *leet(char *str);
+char *cap_string(char *str);
+char *string_toupper(char *str);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
