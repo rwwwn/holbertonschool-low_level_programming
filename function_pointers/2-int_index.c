29f7ae61e1ed69710858739b1ad92ed087f063ee
Your Name <you@example.com>
2025-03-17 09:01:14 +0000

    Add function to search for an integer in an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer in an array
+ * @array: The array to search
+ * @size: The number of elements in the array
+ * @cmp: Pointer to the function used to compare values
+ *
+ * Return: Index of the first matching element, or -1 if no match or invalid size
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp && size > 0)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+}
+return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 void print_name(char *name, void (*f)(char *));
 
