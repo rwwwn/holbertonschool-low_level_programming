4020a84049eaa4d456570de278f8c803fa51f2aa
Your Name <you@example.com>
2025-03-17 08:45:30 +0000

    Add program to print its own name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
