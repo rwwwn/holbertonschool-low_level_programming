8e15aa6dbb0a0301df64478292ffa0cd6eff7a79
rwwwn <ralbaraiki@gmail.com>
2025-03-18 09:16:28 +0000

    Fix print_all: only 2 if statements, aligned left

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,28 +11,33 @@ void print_all(const char * const format, ...)
 va_list args;
 int i = 0;
 char *str, *sep = "";
+char type;
 
 va_start(args, format);
 
 while (format && format[i])
 {
-if (format[i] == 'c')
-printf("%s%c", sep, va_arg(args, int));
-else if (format[i] == 'i')
-printf("%s%d", sep, va_arg(args, int));
-else if (format[i] == 'f')
-printf("%s%f", sep, va_arg(args, double));
-else if (format[i] == 's')
+type = format[i];
+
+if (type == 'c' || type == 'i' || type == 'f' || type == 's')
+{
+printf("%s", sep);
+sep = ", ";
+
+if (type == 'c')
+printf("%c", va_arg(args, int));
+if (type == 'i')
+printf("%d", va_arg(args, int));
+if (type == 'f')
+printf("%f", va_arg(args, double));
+if (type == 's')
 {
 str = va_arg(args, char *);
-if (!str)
-str = "(nil)";
-printf("%s%s", sep, str);
+printf("%s", str ? str : "(nil)");
 }
-sep = ", ";
+}
 i++;
 }
-
 printf("\n");
 va_end(args);
 }
