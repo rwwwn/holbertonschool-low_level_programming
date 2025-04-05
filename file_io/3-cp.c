058dfec09319d141ce34dfe400feffd74fd27d78
rwwwn <ralbaraiki@gmail.com>
2025-04-05 21:23:40 +0000

    Fix: check read() return value and exit 98 on failure

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,21 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 
 #define BUFFER_SIZE 1024
 
 /**
- * main - copies the content of a file to another file
+ * main - copies content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success, exit with error code otherwise
+ * Return: 0 on success, exits with error code otherwise
  */
 int main(int argc, char *argv[])
 {
 int file_from, file_to, r, w;
-char *buffer;
+char buffer[BUFFER_SIZE];
 
 if (argc != 3)
 {
@@ -23,15 +23,10 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
-buffer = malloc(sizeof(char) * BUFFER_SIZE);
-if (!buffer)
-return (1);
-
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buffer);
 exit(98);
 }
 
@@ -39,7 +34,6 @@ file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (file_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buffer);
 close(file_from);
 exit(99);
 }
@@ -50,17 +44,15 @@ w = write(file_to, buffer, r);
 if (w != r)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buffer);
 close(file_from);
 close(file_to);
 exit(99);
 }
 }
 
-if (r == -1)
+if (r == -1) /* This is the crucial fix */
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buffer);
 close(file_from);
 close(file_to);
 exit(98);
@@ -69,17 +61,14 @@ exit(98);
 if (close(file_from) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-free(buffer);
 exit(100);
 }
 
 if (close(file_to) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-free(buffer);
 exit(100);
 }
 
-free(buffer);
 return (0);
 }
