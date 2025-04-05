f0b05189f8caa1f9ac46fee381fb04e33a102c09
rwwwn <ralbaraiki@gmail.com>
2025-04-05 20:55:10 +0000

    Add <main>

--- file_io/main.h
+++ file_io/main.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 #endif
