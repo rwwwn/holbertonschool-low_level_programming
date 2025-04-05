adbc607d7b9462447c09afb686585e550cad81b3
rwwwn <ralbaraiki@gmail.com>
2025-04-05 20:25:01 +0000

    Add function to get value of bit at specific index

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
