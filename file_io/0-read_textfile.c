542eef1fc959a1df8895f2cc8cb95075b8ae6fb1
rwwwn <ralbaraiki@gmail.com>
2025-04-05 21:03:08 +0000

    Fix Betty warnings: read_textfile.c aligned to kernel and Betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,45 +1,49 @@
 #include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
- * @filename: Name of the file to read.
- * @letters: Number of letters it should read and print.
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
  *
- * Return: Actual number of letters read and printed. 0 on failure.
+ * Return: Number of letters printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
- ssize_t fd, r, w;
- char *buffer;
+int fd;
+ssize_t r, w;
+char *buffer;
 
- if (filename == NULL)
-  return (0);
+if (filename == NULL)
+return (0);
 
- buffer = malloc(sizeof(char) * letters);
- if (buffer == NULL)
-  return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
- fd = open(filename, O_RDONLY);
- if (fd == -1)
- {
-  free(buffer);
-  return (0);
- }
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
 
- r = read(fd, buffer, letters);
- if (r == -1)
- {
-  free(buffer);
-  close(fd);
-  return (0);
- }
+r = read(fd, buffer, letters);
+if (r == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
 
- w = write(STDOUT_FILENO, buffer, r);
- free(buffer);
- close(fd);
+w = write(STDOUT_FILENO, buffer, r);
+free(buffer);
+close(fd);
 
- if (w == -1 || w != r)
-  return (0);
+if (w != r)
+return (0);
 
- return (w);
+return (w);
 }
