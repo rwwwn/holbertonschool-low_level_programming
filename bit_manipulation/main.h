a5454d54a495d4dec524e9709ff1d9755a08dcde
rwwwn <ralbaraiki@gmail.com>
2025-04-05 20:33:58 +0000

    Add function to clear bit to 0 at given index

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int clear_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
