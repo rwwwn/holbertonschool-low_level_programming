1f58250af5071d7af0cfe0c8caaca94dd67273fb
rwwwn <ralbaraiki@gmail.com>
2025-04-05 20:36:09 +0000

    Add function to count bits to flip to convert one number to another

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns number of bits to flip to get from n to m
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+ unsigned long int xor;
+ unsigned int count;
+
+ xor = n ^ m;
+ count = 0;
+
+ while (xor)
+ {
+  count += xor & 1;
+  xor >>= 1;
+ }
+
+ return (count);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
