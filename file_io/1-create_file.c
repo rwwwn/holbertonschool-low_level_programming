2f134603494d9bf4ed80dc37c73d808263e3f758
rwwwn <ralbaraiki@gmail.com>
2025-04-05 20:49:06 +0000

    Add function to create file with 0600 permissions and write content

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, w, len = 0;
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+while (text_content[len])
+len++;
+}
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+{
+w = write(fd, text_content, len);
+if (w == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
