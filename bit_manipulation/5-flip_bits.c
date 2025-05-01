6a013553dca93679f00a15959c89aff239806fa8
rwwwn <ralbaraiki@gmail.com>
2025-05-01 12:29:18 +0000

    fixed betty

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -9,17 +9,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
- unsigned long int xor;
- unsigned int count;
-
- xor = n ^ m;
- count = 0;
-
- while (xor)
- {
-  count += xor & 1;
-  xor >>= 1;
- }
-
- return (count);
+unsigned long int xor;
+unsigned int count;
+xor = n ^ m;
+count = 0;
+while (xor)
+{
+count += xor & 1;
+xor >>= 1;
+}
+return (count);
 }
